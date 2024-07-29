1084f49f270ccd8d365749c043f8cdd52d0f3fa1
Matthew Martin <matthewmartin117@gmail.com>
2024-07-29 22:12:39 +0000

    select db

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def create():
     conn.close()
     return "Basketball Table Sucessfully Created"
 
-
+#This route will insert data into the Basketball table that you've created.
 @app.route('/db_insert')
 def inserting():
     # connect to database
@@ -59,7 +59,32 @@ def inserting():
     # return a confirmation string
     return "Basketball Table Succesfully Populated"
 
-
+#This route will query all of the data from the database, and return the information in a table.
+@app.route('/db_select')
+def selecting():
+    #create a connection  
+    conn = psycopg2.connect("postgresql://mattmartin_lab10database_user:u7EzgKpgdZosFadNuNAfnVjIz2USZIPZ@dpg-cqj9ctmehbks73c8agkg-a/mattmartin_lab10database")
+    # create a cursor from the connection
+    cur = conn.cursor()
+    # execute a statement that selects the table
+    cur.execute('''
+        SELECT * FROM Basketball;
+    ''')
+    # assign the results to a variable 
+    records = cur.fetchall()
+    # close the connection
+    conn.close()
+    # create a string to return the response 
+    response_string=""
+    response_string+="<table>"
+    for player in records:
+        response_string+="<tr>"
+        for info in player:
+            response_string+="<td>{}</td>".format(info)
+        response_string+="</tr>"
+    response_string+="</table>"
+    # return the formatted table
+    return response_string
 
     
     

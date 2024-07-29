57cb99379e9d553941c3fb0901886ff0842c2def
Matthew Martin <matthewmartin117@gmail.com>
2024-07-29 21:58:35 +0000

    db_create

--- app.py
+++ app.py
@@ -17,4 +17,25 @@ def test():
     conn = psycopg2.connect("postgresql://mattmartin_lab10database_user:u7EzgKpgdZosFadNuNAfnVjIz2USZIPZ@dpg-cqj9ctmehbks73c8agkg-a/mattmartin_lab10database")
     conn.close()
     return "Database Connection Sucessful"
+
+# db test route 
+@app.route('/db_create')
+def create():
+    # connect to postgreSQL db 
+    conn = psycopg2.connect("postgresql://mattmartin_lab10database_user:u7EzgKpgdZosFadNuNAfnVjIz2USZIPZ@dpg-cqj9ctmehbks73c8agkg-a/mattmartin_lab10database")
+    # create a basketball table 
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+            );
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Sucessfully Created"
+    
+    
     

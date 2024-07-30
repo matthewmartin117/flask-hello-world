1d6993462ba3d453bb9261683060b9d8e7c6c151
Matthew Martin <matthewmartin117@gmail.com>
2024-07-30 14:17:24 +0000

    debugging
    connection

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def hello_world():
 @app.route('/db_test')
 def test():
     # connect to our postgreSQL database
-    conn = psycopg2.connect("postgresql://mattmartin_lab10database_user:u7EzgKpgdZosFadNuNAfnVjIz2USZIPZ@dpg-cqj9ctmehbks73c8agkg-a/mattmartin_lab10database")
+    conn = psycopg2.connect('''postgresql://mattmartin_lab10database_user:u7EzgKpgdZosFadNuNAfnVjIz2USZIPZ@dpg-cqj9ctmehbks73c8agkg-a/mattmartin_lab10database''')
     conn.close()
     return "Database Connection Sucessful"
 
